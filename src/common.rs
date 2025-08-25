c4ac8f28a630f78ce642d164ad624e4dc2a977bb
Henley-Yu <315919507@qq.com>
2025-08-25 23:25:48 +0800

    update api

--- src/common.rs
+++ src/common.rs
@@ -958,7 +958,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rust-admin.starryit.com".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
